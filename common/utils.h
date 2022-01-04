fe11624bfa0a8c83dea55f6da50dda4867302af3
Dominique Martinet <asmadeus@codewreck.org>
2022-01-04 12:32:36 +0900

    Repo move

--- common/utils.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: LGPL-3.0-or-later */
-
-#ifndef COORDINATOOL_UTILS_H
-#define COORDINATOOL_UTILS_H
-
-#define UNUSED __attribute__((unused))
-
-#endif
--- makerpms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VERSION=$(awk '/Version/ { print $2 }' coordinatool.spec)
-
-# alternatively, meson build && ninja -C build dist can be used
-git archive --prefix=coordinatool-$VERSION/ HEAD |
-	xz > coordinatool-$VERSION.tar.xz
-
-rpmbuild -ta coordinatool-$VERSION.tar.xz
